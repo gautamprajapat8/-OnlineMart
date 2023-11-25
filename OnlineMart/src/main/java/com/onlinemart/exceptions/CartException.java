47ee854f7b1cfe7f86dea425637df83791ad3b76
Gautam kumar <gautamk1336@gmail.com>
2023-11-25 19:35:10 +0530

    Update CartException.java

--- OnlineMart/src/main/java/com/onlinemart/exceptions/CartException.java
+++ OnlineMart/src/main/java/com/onlinemart/exceptions/CartException.java
@@ -4,7 +4,8 @@ public class CartException extends Exception {
 
 	private static final long serialVersionUID = 3473613894783658320L;
 
-	public CartException() {
+	public CartException() 
+	{
 		// TODO Auto-generated constructor stub
 	}
 	
